a8b42fab74911275298d7ce285ad1a0415a775f2
@louis-pvs <louis-jx@jxinternet.com>
2019-01-29 14:37:26 +0800

    some basic setup for react

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-function component() {
-  var element = document.createElement("div");
-  var button = document.createElement("button");
-  var br = document.createElement("br");
+import React from "react";
+import ReactDOM from "react-dom";
 
-  button.innerHTML = "Click me";
-  element.appendChild(button);
-  element.appendChild(br);
-
-  button.onclick = e => {
-    e.preventDefault();
-    import(/* webpackChunkName: 'logger' */ "./logger").then(logger =>
-      logger("hello world!")
-    );
-  };
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(<p>Hello world</p>, document.getElementById("root"));
 
 if (typeof module.hot !== "undefined") {
   module.hot.accept();
